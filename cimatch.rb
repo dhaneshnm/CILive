313597b2bed0a110b4e4ff1324614a91a0a20da7
dragon364 <dhanesh.n.m19@gmail.com>
2014-05-06 18:45:35 -0400

    initial commit

---
+++ cimatch.rb
@@ -0,0 +1,64 @@
+require 'open-uri'
+require 'json'
+require 'asciiart'
+source = "http://www.espncricinfo.com/ci/engine/match/706767.json"
+def jsonisque(source)
+	last = nil
+	while(true)
+		match_data = JSON.load(open(source))
+		comment = match_data["comms"][0]["ball"][0]
+		if comment["overs_unique"] != last
+		    system('history -c')
+		    event = "waiting ...."
+		    if(!comment["event"].nil?)
+		     event = comment["event"].strip.downcase
+		    end
+		    if(event == "four" || event == "six" || event == "out" )
+		      show_umpire(event)
+		    elsif(event == "no run")
+		      show_umpire("norun")
+		    else
+		      show_umpire("other")
+		    end
+		    puts match_data["match"]["team1_name"]+" v/s "+match_data["match"]["team2_name"]
+		    puts match_data["match"]["current_summary"]
+			puts comment["players"]
+			system "artii ' "+event+"'"
+			system('say -v "Alex" "'+event+'"')
+			if(! (event == "waiting ...."))
+				system('say -v "Junior" "'+comment["players"]+'"')
+				system('say -v "Alex" "'+comment["text"]+'"')
+			end
+		    puts match_data["live"]["status"]
+			last = comment["overs_unique"]
+		else
+		  system "artii 'waiting for the action'"
+		  puts match_data["match"]["current_summary"]
+		  puts "most recent over : "+recent_over_string(match_data)
+		  puts match_data["live"]["status"]
+		  puts comment["text"]
+		  puts comment["post_text"]
+		end
+		sleep(1)
+	end
+end
+def show_umpire(signal)
+	path = ""
+	signal_image = signal+".jpeg"
+	fullpath = path+signal_image
+	score = AsciiArt.new(fullpath)
+	puts score.to_ascii_art
+end
+def recent_over_string(match_data)
+	recent = ""
+    match_data["live"]["recent_overs"].first.each do |ball|
+	actual = "0"
+		if(!ball["ball"].include?('&'))
+	 	 actual = ball["ball"]
+		end
+	score = ball["extras"]+actual
+	recent = recent+" "+score
+	end
+	recent
+end
+jsonisque(source)
---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'json'
+require 'asciiart'
+source = "http://www.espncricinfo.com/indian-premier-league-2014/engine/match/733973.json"
+match_data = JSON.load(open(source))
+recent = ""
+match_data["live"]["recent_overs"].first.each do |ball|
+	actual = "0"
+	if(!ball["ball"].include?('&'))
+	  actual = ball["ball"]
+	end
+	score = ball["extras"]+actual
+	recent = recent+" "+score
+end
+puts recent
